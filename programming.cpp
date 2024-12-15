62925781993513db268f9d6b09b7e5024881edb4
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-12-16 02:16:25 +0600

    again

--- programming.cpp
+++ programming.cpp
@@ -121,47 +121,47 @@
 // }
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int N;
-    cin >> N;
+// int main() {
+//     int N;
+//     cin >> N;
 
-    for (int i = 0; i < N / 2; ++i) {
-        for (int j = 0; j < N; ++j) {
-            if (j == i) {
-                cout << "\\";
-            } else if (j == N - i - 1) {
-                cout << "/";
-            } else {
-                cout << " ";
-            }
-        }
-        cout << endl;
-    }
+//     for (int i = 0; i < N / 2; ++i) {
+//         for (int j = 0; j < N; ++j) {
+//             if (j == i) {
+//                 cout << "\\";
+//             } else if (j == N - i - 1) {
+//                 cout << "/";
+//             } else {
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
 
-    for (int j = 0; j < N; ++j) {
-        if (j == N / 2) {
-            cout << "X";
-        } else {
-            cout << " ";
-        }
-    }
-    cout << endl;
+//     for (int j = 0; j < N; ++j) {
+//         if (j == N / 2) {
+//             cout << "X";
+//         } else {
+//             cout << " ";
+//         }
+//     }
+//     cout << endl;
 
-    for (int i = N / 2 - 1; i >= 0; --i) {
-        for (int j = 0; j < N; ++j) {
-            if (j == i) {
-                cout << "/";
-            } else if (j == N - i - 1) {
-                cout << "\\";
-            } else {
-                cout << " ";
-            }
-        }
-        cout << endl;
-    }
+//     for (int i = N / 2 - 1; i >= 0; --i) {
+//         for (int j = 0; j < N; ++j) {
+//             if (j == i) {
+//                 cout << "/";
+//             } else if (j == N - i - 1) {
+//                 cout << "\\";
+//             } else {
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
